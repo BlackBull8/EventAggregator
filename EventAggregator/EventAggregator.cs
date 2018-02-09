e23858fc35cdc1eaaee6b187d72917ace9c9b904
hongjunbin <hongjunbin@cvte.com>
2018-02-09 17:38:39 +0800

    [modify]修改EventAggregator的访问级别

--- EventAggregator/EventAggregator.cs
+++ EventAggregator/EventAggregator.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace EventAggregator
 {
-    public class EventAggregator
+    internal class EventAggregator
     {
         private readonly Dictionary<string, List<EventMessageHandler>> _handlers;
 
